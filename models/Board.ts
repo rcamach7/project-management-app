0a640b3c574c219d0c0a442533676a25e31ca5d9
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-09-10 22:04:15 -0700

    refactor: Convert mongoose schemas to regular types

--- models/Board.ts
+++ models/Board.ts
@@ -1,11 +1,39 @@
-import { model, Schema, models } from 'mongoose';
+import { Schema } from 'mongoose';
 
-const Board = new Schema({
+const Comment = {
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  text: { type: String, required: true, minLength: 3, maxLength: 255 },
+};
+
+const Card = {
   title: { type: String, required: true, minLength: 3, maxLength: 50 },
   description: { type: String, required: true, minLength: 3, maxLength: 255 },
-  cards: [{ type: Schema.Types.ObjectId, ref: 'Card' }],
-});
+  labels: [
+    {
+      type: String,
+      enum: [
+        'BACKEND',
+        'FRONTEND',
+        'FEAT',
+        'REFACTOR',
+        'TEST',
+        'PERF',
+        'STYLE',
+        'ASSET',
+        'DOC',
+        'CI',
+        'CHORE',
+        'WIP',
+      ],
+    },
+  ],
+  comments: [Comment],
+};
 
-const BoardModel = models.Board || model('Board', Board);
+const Board = {
+  title: { type: String, required: true, minLength: 3, maxLength: 50 },
+  description: { type: String, required: true, minLength: 3, maxLength: 255 },
+  cards: [Card],
+};
 
-export default BoardModel;
+export default Board;
