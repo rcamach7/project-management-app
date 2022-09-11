d03552b161960072717b559b099ff1240bb2060a
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-09-10 22:04:15 -0700

    refactor: Convert mongoose schemas to regular types

--- models/Workspace.ts
+++ models/Workspace.ts
@@ -1,10 +1,11 @@
 import { model, models, Schema } from 'mongoose';
+import Board from './Board';
 
 const Workspace = new Schema({
   name: { type: String, required: true, minLength: 3, maxLength: 50 },
   description: { type: String, required: true, minLength: 3, maxLength: 255 },
-  boards: [{ type: Schema.Types.ObjectId, ref: 'Board' }],
   users: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  boards: [Board],
 });
 
 const WorkspaceModel = models.Workspace || model('Workspace', Workspace);
