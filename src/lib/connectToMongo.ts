1794c1c9ee3ff5121e383af267a51a86cb567d9c
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-10-05 12:31:47 -0700

    chore: Refactor function name to match file name

--- src/lib/connectToMongo.ts
+++ src/lib/connectToMongo.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 
-const connectMongo = async () => mongoose.connect(process.env.MONGODB_URI);
+const connectToMongo = async () => mongoose.connect(process.env.MONGODB_URI);
 
-export default connectMongo;
+export default connectToMongo;
