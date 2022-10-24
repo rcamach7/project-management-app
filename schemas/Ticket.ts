d2265fe0ab9dd3559b1125637deed67f02c3d5a1
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-10-23 22:25:29 -0700

    feat: Create crud actions for ticket schema

--- schemas/Ticket.ts
+++ schemas/Ticket.ts
@@ -1,6 +1,7 @@
 import { model, Schema, models } from 'mongoose';
 
 const Ticket = new Schema({
+  board_id: { type: Schema.Types.ObjectId, ref: 'Board', required: true },
   title: { type: String, required: true, minLength: 3, maxLength: 50 },
   description: {
     type: String,
@@ -28,6 +29,22 @@ const Ticket = new Schema({
       required: true,
     },
   ],
+});
+
+Ticket.pre('findOneAndDelete', async function (next) {
+  const ticketToBeDeleted = await this.model.findOne({
+    _id: this.getQuery()._id,
+  });
+
+  // Delete ticket from board
+  await model('Board').updateOne(
+    { _id: ticketToBeDeleted.board_id },
+    {
+      $pull: { tickets: ticketToBeDeleted._id },
+    }
+  );
+
+  next();
 });
 
 const TicketSchema: any = models.Ticket || model('Ticket', Ticket);
--- src/pages/api/workspace/board/ticket/[tid].ts
+++ src/pages/api/workspace/board/ticket/[tid].ts
@@ -1,6 +1,11 @@
 import { unstable_getServerSession } from 'next-auth/next';
 import { authOptions } from '@/auth/[...nextauth]';
 import { AppSession } from 'models/global.types';
+import {
+  getTicketById,
+  deleteTicketById,
+  updateTicketFieldsById,
+} from 'controllers/ticketController';
 
 export default async (req, res) => {
   const session: AppSession = await unstable_getServerSession(
@@ -10,15 +15,49 @@ export default async (req, res) => {
   );
   if (!session) res.status(401).json({ message: 'Unauthorized' });
 
+  const { tid } = req.query;
+  if (!tid)
+    return res
+      .status(400)
+      .json({ message: 'Please provide a valid ticket ID' });
+
   const { method } = req;
   switch (method) {
     case 'GET':
       try {
-        res.json({ msg: 'GET request on api/workspace/board/ticket/[tid]' });
+        const ticket = await getTicketById(tid);
+        res.json(ticket);
       } catch (error) {
         res.status(500).json({ error });
       }
       break;
+
+    case 'PUT':
+      const { title, description, labels } = req.body;
+      if (!title && !description && !labels)
+        return res.status(400).json({ message: 'Missing required fields' });
+
+      try {
+        const ticket = await updateTicketFieldsById(tid, {
+          title,
+          description,
+          labels,
+        });
+        res.json(ticket);
+      } catch (error) {
+        res.status(500).json({ error });
+      }
+      break;
+
+    case 'DELETE':
+      try {
+        const ticket = await deleteTicketById(tid);
+        res.json(ticket);
+      } catch (error) {
+        res.status(500).json({ error });
+      }
+      break;
+
     default:
       res.setHeader('Allow', ['GET']);
       res.status(405).end(`Method ${method} Not Allowed`);
