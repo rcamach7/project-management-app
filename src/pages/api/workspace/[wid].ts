5fe30f654240e371efbd73b53521effa618afd64
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-10-28 15:12:08 -0700

    refactor: Update workspace endpoint to use dynamic querieS

--- src/pages/api/workspace/[wid].ts
+++ src/pages/api/workspace/[wid].ts
@@ -1,5 +1,10 @@
 import { unstable_getServerSession } from 'next-auth/next';
 import { NextApiRequest, NextApiResponse } from 'next';
+import {
+  createNewWorkspace,
+  getWorkspaceById,
+  deleteWorkspace,
+} from 'controllers/workspaceController';
 import { authOptions } from '@/auth/[...nextauth]';
 import { AppSession } from 'models/global.types';
 
@@ -11,6 +16,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   );
   if (!session) res.status(401).json({ message: 'Unauthorized' });
 
+  const { wid } = req.query;
+  if (!wid || Array.isArray(wid))
+    return res.status(400).json({ message: 'Missing valid workspace Id' });
+
+  const { name, description } = req.body;
   const { method } = req;
   switch (method) {
     case 'GET':
@@ -20,8 +30,44 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         res.status(500).json({ message: 'Error retrieving workspace', error });
       }
       break;
+
+    case 'POST':
+      try {
+        // const { name, description } = req.body;
+        const workspace = await createNewWorkspace(
+          {
+            name: name ? name : 'Untitled Workspace',
+            description: description ? description : '',
+          },
+          session.user._id
+        );
+
+        res.json(workspace);
+      } catch (error) {
+        res.status(500).json({ message: 'Error updating workspace', error });
+      }
+      break;
+
+    case 'DELETE':
+      try {
+        const workspaceToDelete = await getWorkspaceById(wid);
+        if (workspaceToDelete.owner.toString() !== session.user._id) {
+          res.status(401).json({
+            message: 'You are not authorized to delete this workspace',
+          });
+        }
+
+        const workspace = await deleteWorkspace(wid);
+        res.json(workspace);
+      } catch (error) {
+        res.status(500).json({
+          message: 'Error ocurred deleting workspace',
+          error,
+        });
+      }
+      break;
     default:
-      res.setHeader('Allow', ['GET']);
+      res.setHeader('Allow', ['GET', 'POST', 'DELETE']);
       res.status(405).end(`Method ${method} Not Allowed`);
   }
 };
