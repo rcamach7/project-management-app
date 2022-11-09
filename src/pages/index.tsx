009ae6961a9f631a46cdb3d46a94a145361e4dc7
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-11-09 14:54:07 -0800

    feat: Create extended Boc component and barebones ActionButton, and add them to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import Head from 'next/head';
 import { Box } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
-import { PageTitle, FeatureCard } from '@/components/index';
+import {
+  PageTitle,
+  FeatureCard,
+  ActionButton,
+  CenteredBox,
+} from '@/components/index';
 
 export async function getStaticProps() {
   return {
@@ -49,13 +54,33 @@ export default function Home({ featuresList }: Props) {
         <title>Flow: Sign In</title>
       </Head>
 
-      <Box sx={{ p: 2 }}>
-        <PageTitle
-          title="Flow"
-          subheading="Collaborative Project Management Tool"
-        />
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          p: 2,
+          minHeight: '100vh',
+        }}
+      >
+        <CenteredBox flex={1}>
+          <PageTitle
+            title="Flow"
+            subheading="Collaborative Project Management Tool"
+          />
+        </CenteredBox>
 
-        <Grid container spacing={1} justifyContent="space-evenly" sx={{ p: 1 }}>
+        <CenteredBox flex={1}>
+          <ActionButton text="Sign In / Get Started" />
+        </CenteredBox>
+
+        <Grid
+          container
+          spacing={1}
+          justifyContent="space-evenly"
+          sx={{ flex: 2, p: 1 }}
+        >
           {featuresList.map((feature) => (
             <Grid xs={5} md={4} lg={3}>
               <FeatureCard key={feature.title} {...feature} />
