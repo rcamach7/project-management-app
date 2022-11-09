31c8c46d31322171dc7fa51b2613c95bc7f59844
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-11-09 15:09:35 -0800

    style: Improve ActionButton styling and add additional props for customization

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -2,11 +2,22 @@ import { Button } from '@mui/material';
 
 interface Props {
   text: string;
+  variant: 'contained' | 'outlined';
+  onClick?: () => void;
 }
 
-export default function ActionButton({ text }: Props) {
+export default function ActionButton({ text, variant, onClick }: Props) {
   return (
-    <Button variant="contained" size="large">
+    <Button
+      variant={variant}
+      size="large"
+      sx={{
+        color: 'text.primary',
+        border: 'solid 1px',
+        borderColor: 'secondary.main',
+      }}
+      onClick={onClick}
+    >
       {text}
     </Button>
   );
