ee099837eb275c85ee49575401e516bd62898171
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-11-10 17:18:52 -0800

    feat: Add endpoint that retrives all workspaces users are a part of

--- src/pages/api/workspace/index.ts
+++ src/pages/api/workspace/index.ts
@@ -1,9 +1,11 @@
 import { unstable_getServerSession } from 'next-auth/next';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { authOptions } from '@/auth/[...nextauth]';
-import { createNewWorkspace } from 'controllers/workspaceController';
+import {
+  createNewWorkspace,
+  getUserWorkspaces,
+} from 'controllers/workspaceController';
 import { AppSession } from 'models/global.types';
-import { Workspace } from 'schemas';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const session: AppSession = await unstable_getServerSession(
@@ -16,10 +18,9 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { name, description } = req.body;
   const { method } = req;
   switch (method) {
-    // TODO: Delete this endpoint before production - only for testing
     case 'GET':
       try {
-        const workspaces = await Workspace.find();
+        const workspaces = await getUserWorkspaces(session.user._id);
 
         res.json(workspaces);
       } catch (error) {
