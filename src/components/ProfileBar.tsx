00ce09e2b2d475b791c7a3f90277ec1ecca0723b
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-11-11 13:18:56 -0800

    wip: Create basic profile bar without button

--- src/components/ProfileBar.tsx
+++ src/components/ProfileBar.tsx
@@ -1,5 +1,5 @@
 import { AppSession } from 'models/global';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { CenteredBox } from '@/components/index';
 import Image from 'next/image';
 
@@ -9,18 +9,42 @@ interface Props {
 
 export default function ProfileBar({ user }: Props) {
   return (
-    <CenteredBox sx={{ flexDirection: 'row', border: 1 }}>
+    <Box
+      sx={{
+        minWidth: '300px',
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        gap: 1,
+        border: 1,
+        p: 1,
+      }}
+    >
       <Box
         sx={{
-          width: '5em',
-          height: '5em',
+          width: { xs: '5.5em', sm: '6em', md: '6.5em' },
+          height: { xs: '5.5em', sm: '6em', md: '6.5em' },
           overflow: 'hidden',
-          borderRadius: 10,
+          borderRadius: '50%',
         }}
       >
         <Image src={user.image} width="50" height="50" layout="responsive" />
       </Box>
-      <Box>Box</Box>
-    </CenteredBox>
+      <Box sx={{ p: 0.5 }}>
+        <Typography sx={{ fontSize: { xs: '1em', sm: '1.1em', md: '1.2em' } }}>
+          Hello,
+        </Typography>
+        <Typography
+          sx={{
+            fontWeight: 'bold',
+            fontSize: { xs: '1.2em', sm: '1.4em', md: '1.6em' },
+          }}
+        >
+          {user.name}
+        </Typography>
+        <Typography>Sign Out</Typography>
+      </Box>
+    </Box>
   );
 }
