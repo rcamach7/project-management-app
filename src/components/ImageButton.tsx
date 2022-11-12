5bdb30ca5b7c3a54b799e2827ddcdd7da07833e4
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-11-11 19:36:07 -0800

    wip: Add SVGs and styling to ImageButton

--- src/components/ImageButton.tsx
+++ src/components/ImageButton.tsx
@@ -1,21 +1,35 @@
 import { Box, Typography } from '@mui/material';
+import Image from 'next/image';
 import { CenteredBox } from '@/components/index';
 
 interface Props {
   text: string;
+  image: string;
+  onClick?: () => void;
 }
 
-export default function ImageButton({ text }: Props) {
+export default function ImageButton({ text, image }: Props) {
   return (
     <CenteredBox sx={{ height: '100px', gap: 0.5 }}>
       <Box
         sx={{
+          cursor: 'pointer',
           width: '5em',
           height: '5em',
-          background: 'red',
+          // TODO: Pull from theme
+          background: '#112240',
           borderRadius: '50%',
+          transition: 'all .2s ease-in-out',
+          '&:hover': {
+            transform: 'scale(1.1)',
+            backgroundColor: 'text.secondary',
+          },
+          border: 1,
+          padding: 2,
         }}
-      />
+      >
+        <Image src={image} width="20" height="20" layout="responsive" />
+      </Box>
       <Typography>{text}</Typography>
     </CenteredBox>
   );
