92bd6ae4c8bbfe5054946c6db19c24ebe8490d2b
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-11-13 13:55:11 -0800

    feat: Create Link anchor and attach to WorkspaceSumary

---
+++ src/components/atoms/NextLinkAnchor.tsx
@@ -0,0 +1,44 @@
+import { forwardRef } from 'react';
+import NextLink, { LinkProps as NextLinkProps } from 'next/link';
+
+interface NextLinkComposedProps
+  extends Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, 'href'>,
+    Omit<
+      NextLinkProps,
+      'href' | 'as' | 'onClick' | 'onMouseEnter' | 'onTouchStart'
+    > {
+  to: NextLinkProps['href'];
+  linkAs?: NextLinkProps['as'];
+}
+
+const NextLinkAnchor = forwardRef<HTMLAnchorElement, NextLinkComposedProps>(
+  function (props, ref) {
+    const {
+      to,
+      linkAs,
+      replace,
+      scroll,
+      shallow,
+      prefetch = false,
+      locale,
+      ...attributes
+    } = props;
+
+    return (
+      <NextLink
+        href={to}
+        prefetch={prefetch}
+        as={linkAs}
+        replace={replace}
+        scroll={scroll}
+        shallow={shallow}
+        passHref
+        locale={locale}
+      >
+        <a style={{ textDecoration: 'none' }} ref={ref} {...attributes} />
+      </NextLink>
+    );
+  }
+);
+
+export default NextLinkAnchor;
--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -2,3 +2,4 @@ export { default as ActionButton } from './ActionButton';
 export { default as ImageButton } from './ImageButton';
 export { default as FeatureCard } from './FeatureCard';
 export { default as PageTitle } from './PageTitle';
+export { default as NextLinkAnchor } from './NextLinkAnchor';
