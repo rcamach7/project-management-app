14fabac2bd2d5504ab547ef431f48f4117330c13
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-11-12 16:36:02 -0800

    feat: Create basic WorkspaceSummary component with sub-components

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,5 @@ export { default as ActionButton } from './ActionButton';
 export { default as CenteredBox } from './CenteredBox';
 export { default as ProfileBar } from './ProfileBar';
 export { default as ImageButton } from './ImageButton';
+
+export { default as WorkspaceSummary } from './WorkspaceSummary/WorkspaceSummary';
