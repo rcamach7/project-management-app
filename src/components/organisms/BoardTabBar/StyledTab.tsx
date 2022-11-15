ccc4796135677c840b738bcbbd3f2e8864234aff
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-11-14 17:09:08 -0800

    style: Improve styling on tabs and container

--- src/components/organisms/BoardTabBar/StyledTab.tsx
+++ src/components/organisms/BoardTabBar/StyledTab.tsx
@@ -10,7 +10,7 @@ const StyledTab = styled((props: StyledTabProps) => (
   <Tab disableRipple {...props} />
 ))(({ theme }) => ({
   textTransform: 'none',
-  fontWeight: theme.typography.fontWeightRegular,
+  fontWeight: 'bold',
   fontSize: theme.typography.pxToRem(15),
   marginRight: theme.spacing(1),
   color: 'rgba(255, 255, 255, 0.7)',
