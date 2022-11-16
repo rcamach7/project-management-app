a9154199aadf9d343fce98747391f1c499e00d78
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-11-15 22:30:18 -0800

    feat: Create ConfirmDeleteDialog form

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -3,3 +3,4 @@ export { default as ImageButton } from './ImageButton';
 export { default as FeatureCard } from './FeatureCard';
 export { default as PageTitle } from './PageTitle';
 export { default as NextLinkAnchor } from './NextLinkAnchor';
+export { default as ConfirmDeleteDialog } from './ConfirmDeleteDialog';
--- src/components/organisms/TicketsDisplay/Ticket.tsx
+++ src/components/organisms/TicketsDisplay/Ticket.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import { Ticket as TicketType } from 'models/client';
 import { Card, CardActions as TicketActions, Button } from '@mui/material';
+import { ConfirmDeleteDialog } from '@/components/atoms/index';
 import TicketContent from './TicketContent';
 
 interface Props {
@@ -8,20 +10,44 @@ interface Props {
 
 export default function Ticket({ ticket }: Props) {
   const { _id, board_id, description, labels, title } = ticket;
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+
   return (
-    <Card sx={{ border: 1 }}>
-      <TicketContent description={description} title={title} labels={labels} />
-      <TicketActions>
-        <Button size="small" sx={{ color: 'secondary.main' }}>
-          Edit
-        </Button>
-        <Button size="small" sx={{ color: 'secondary.main' }}>
-          Move to...
-        </Button>
-        <Button size="small" sx={{ color: 'red' }}>
-          Delete
-        </Button>
-      </TicketActions>
-    </Card>
+    <>
+      <Card sx={{ border: 1 }}>
+        <TicketContent
+          description={description}
+          title={title}
+          labels={labels}
+        />
+        <TicketActions>
+          <Button size="small" sx={{ color: 'secondary.main' }}>
+            Edit
+          </Button>
+          <Button size="small" sx={{ color: 'secondary.main' }}>
+            Move to...
+          </Button>
+          <Button
+            size="small"
+            sx={{ color: 'red' }}
+            onClick={() => setShowDeleteConfirmation((DC) => !DC)}
+          >
+            Delete
+          </Button>
+        </TicketActions>
+      </Card>
+      {showDeleteConfirmation && (
+        <ConfirmDeleteDialog
+          title={`Are you sure you want to delete this ticket? (${title})`}
+          content={`This will delete the ticket from the board and all of its data will be lost.`}
+          open={showDeleteConfirmation}
+          handleClose={() => setShowDeleteConfirmation(false)}
+          handleConfirm={() => {
+            // delete ticket function here
+            setShowDeleteConfirmation(false);
+          }}
+        />
+      )}
+    </>
   );
 }
