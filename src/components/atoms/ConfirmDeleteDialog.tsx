44fde8a19831f877a808adc72b3cab34c62774f2
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-11-17 16:45:04 -0800

    fix: Remove stock button on Dialog that's not needed

--- src/components/atoms/ConfirmDeleteDialog.tsx
+++ src/components/atoms/ConfirmDeleteDialog.tsx
@@ -30,29 +30,26 @@ export default function ConfirmDeleteDialog({
   };
 
   return (
-    <div>
-      <Button variant="outlined">Open alert dialog</Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            {content}
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button sx={{ color: 'text.primary' }} onClick={handleClose}>
-            Cancel
-          </Button>
-          <Button sx={{ color: 'red' }} onClick={handleSubmit} autoFocus>
-            Delete
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          {content}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button sx={{ color: 'text.primary' }} onClick={handleClose}>
+          Cancel
+        </Button>
+        <Button sx={{ color: 'red' }} onClick={handleSubmit} autoFocus>
+          Delete
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 }
