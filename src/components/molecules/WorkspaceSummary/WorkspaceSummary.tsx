72d8b7e4b77dbfbd230555f1ee404a1d2796cce5
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-11-19 19:36:40 -0800

    wip: Add buttons to Workspace summary and connect the launch button

--- src/components/molecules/WorkspaceSummary/WorkspaceSummary.tsx
+++ src/components/molecules/WorkspaceSummary/WorkspaceSummary.tsx
@@ -1,7 +1,14 @@
 import { WorkspaceSummary as WorkspaceSummaryType } from 'models/global';
-import { Card, CardContent, Typography, CardActionArea } from '@mui/material';
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from '@mui/material';
 import UsersSummary from './UsersSummary';
-import { NextLinkAnchor } from '@/components/atoms/index';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Link from 'next/link';
 
 interface Props {
   workspace: WorkspaceSummaryType;
@@ -12,26 +19,41 @@ export default function WorkspaceSummary({ workspace }: Props) {
 
   return (
     <Card
-      component={NextLinkAnchor}
-      to={`workspace/${workspace._id}`}
-      sx={{ width: 325, height: 175 }}
+      sx={{ width: 325, height: 200, display: 'flex', flexDirection: 'column' }}
     >
-      <CardActionArea sx={{ height: '100%' }}>
-        <CardContent
-          sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}
+      <CardContent sx={{ display: 'flex', flexDirection: 'column', pb: 0 }}>
+        <Typography variant="caption" component="p" sx={{ fontSize: 10 }}>
+          {_id}
+        </Typography>
+        <Typography gutterBottom variant="h6" component="p">
+          {name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+        <UsersSummary users={users} owner={owner} />
+      </CardContent>
+      <CardActions sx={{ mt: 'auto' }}>
+        <Button size="small" sx={{ color: 'secondary.main' }}>
+          <Link href={`workspace/${_id}`}>
+            <Typography
+              sx={{ color: 'secondary.main', pl: 1, cursor: 'pointer' }}
+            >
+              Launch
+            </Typography>
+          </Link>
+        </Button>
+        <Button size="small" sx={{ color: 'secondary.main' }}>
+          Edit
+        </Button>
+        <Button
+          size="small"
+          sx={{ color: 'red', fontSize: 13 }}
+          startIcon={<DeleteIcon />}
         >
-          <Typography variant="caption" component="p" sx={{ fontSize: 10 }}>
-            {_id}
-          </Typography>
-          <Typography gutterBottom variant="h6" component="p">
-            {name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {description}
-          </Typography>
-          <UsersSummary users={users} owner={owner} />
-        </CardContent>
-      </CardActionArea>
+          Delete
+        </Button>
+      </CardActions>
     </Card>
   );
 }
