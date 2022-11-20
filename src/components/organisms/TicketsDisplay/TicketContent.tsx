26f168e51990a3810100b41e4fccee035d2d4c6f
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-11-19 16:25:12 -0800

    refactor: Reduce prop declatations and add Ticket id to title

--- src/components/organisms/TicketsDisplay/TicketContent.tsx
+++ src/components/organisms/TicketsDisplay/TicketContent.tsx
@@ -1,20 +1,32 @@
-import { LabelsEnum } from 'models/client';
+import { Ticket as TicketType } from 'models/client';
 import { Box, CardContent, Typography } from '@mui/material';
 import LabelsDisplay from './LabelsDisplay';
 
 interface Props {
-  labels: LabelsEnum[];
-  title: string;
-  description: string;
+  ticket: TicketType;
 }
 
-export default function TicketContent({ labels, title, description }: Props) {
+export default function TicketContent({ ticket }: Props) {
+  const { title, description, labels, _id } = ticket;
+  const lastFourOfId = _id.slice(-4);
+
   return (
     <CardContent sx={{ py: 1, px: 1.5 }}>
       <LabelsDisplay labels={labels} mobile={true} />
       <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
         <Typography gutterBottom variant="h5" component="div">
           {title}
+          <Typography
+            variant="body2"
+            component="span"
+            sx={{
+              fontSize: 16,
+              color: 'text.secondary',
+              pl: 1,
+            }}
+          >
+            #{lastFourOfId}
+          </Typography>
         </Typography>
         <LabelsDisplay labels={labels} mobile={false} />
       </Box>
