3e65c8b61ee57d1c499a04edcfb5ee6a011f4316
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-11-19 17:17:54 -0800

    wip: Bootstrap basic form for Workpsace CRUD actions

---
+++ src/components/molecules/WorkspaceForm/Form.tsx
@@ -0,0 +1,96 @@
+import { useState } from 'react';
+import { Workspace } from 'models/client';
+import { Box, Button, InputBase, Typography, InputLabel } from '@mui/material';
+
+interface Props {
+  action: 'CREATE' | 'EDIT';
+  workspace?: Workspace;
+  handleClose: () => void;
+  handleWorkspaceFormAction: () => void;
+}
+
+export default function Form({
+  action,
+  workspace,
+  handleClose,
+  handleWorkspaceFormAction,
+}: Props) {
+  const [formDetails, setFormDetails] = useState({
+    title: workspace ? workspace.name : '',
+    description: workspace ? workspace.description : '',
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormDetails({ ...formDetails, [name]: value });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    handleClose();
+    // handleWorkspaceFormAction();
+  };
+
+  return (
+    <Box component="form" onSubmit={handleSubmit} sx={{ pb: 2 }}>
+      <Typography variant="h5" sx={{ fontWeight: 'bold' }} textAlign="center">
+        {action === 'CREATE' ? 'Create ' : 'Edit '}Workspace
+      </Typography>
+
+      <InputLabel shrink htmlFor="workspace-title-input">
+        Workspace Title
+      </InputLabel>
+      <InputBase
+        id="workspace-title-input"
+        placeholder="enter workspace title"
+        name="title"
+        onChange={handleInputChange}
+        value={formDetails.title}
+        sx={{ border: 1, px: 1, borderRadius: 1, mb: 2 }}
+        inputProps={{ minLength: 3, maxLength: 100 }}
+        fullWidth
+        required
+      />
+
+      <InputLabel shrink htmlFor="workspace-description-input">
+        Workspace Description
+      </InputLabel>
+      <InputBase
+        id="workspace-description-input"
+        name="description"
+        placeholder="enter brief description"
+        onChange={handleInputChange}
+        value={formDetails.description}
+        sx={{ border: 1, px: 1, borderRadius: 1, mb: 2 }}
+        inputProps={{ minLength: 3, maxLength: 255 }}
+        fullWidth
+        required
+        multiline
+      />
+
+      <Button
+        variant="outlined"
+        sx={{
+          color: 'secondary.main',
+          border: 1,
+          borderColor: 'secondary.main',
+          p: '2px 4px',
+          mr: 2,
+        }}
+        type="submit"
+      >
+        Submit
+      </Button>
+      <Button
+        variant="text"
+        sx={{
+          color: 'red',
+          p: '2px 4px',
+        }}
+        onClick={handleClose}
+      >
+        Cancel
+      </Button>
+    </Box>
+  );
+}
