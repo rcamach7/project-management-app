2efcf2aafe225df38f2d917a9687ab3b6bea19d4
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-01-13 15:15:00 -0800

    wip: Implement basic MUI Modal wil functionaling toggle on and off screen

--- src/components/atoms/TemplateSelection.tsx
+++ src/components/atoms/TemplateSelection.tsx
@@ -1,5 +1,39 @@
-import React from 'react';
+import { Dispatch, SetStateAction } from 'react';
+import { Modal, Typography, Box } from '@mui/material';
 
-export default function TemplateSelection() {
-  return <p>Template Selection</p>;
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 'clamp(300px, 95%, 640px)',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+interface Props {
+  setShowTemplateSelection: Dispatch<SetStateAction<boolean>>;
+}
+
+export default function TemplateSelection({ setShowTemplateSelection }: Props) {
+  return (
+    <Modal
+      open={true}
+      onClose={() => setShowTemplateSelection((SS) => !SS)}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+      keepMounted
+    >
+      <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          Text in a modal
+        </Typography>
+        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+        </Typography>
+      </Box>
+    </Modal>
+  );
 }
