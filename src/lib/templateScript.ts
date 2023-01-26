e618e9e978f4a5216d4fb401d8ed3b977b22640f
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2023-01-25 19:03:41 -0800

    wip: Group creation of resources vs updating database calls

--- src/lib/templateScript.ts
+++ src/lib/templateScript.ts
@@ -1,6 +1,8 @@
 import { createNewBoard } from 'controllers/boardController';
 import { createNewTicket } from 'controllers/ticketController';
 import { createNewWorkspace } from 'controllers/workspaceController';
+import { Board, Workspace } from 'schemas';
+import { Types } from 'mongoose';
 
 export const templateOptions = ['software project', 'basic'];
 export const createTemplate = async (template: string, userId: string) => {
@@ -14,54 +16,61 @@ export const createTemplate = async (template: string, userId: string) => {
           },
           userId
         );
-
         const backlogBoard = await createNewBoard(
           'Backlog',
           'Backlog of tickets',
           workspace._id
         );
-        await createNewTicket(
+        const ticket = await createNewTicket(
           backlogBoard._id,
           'Tasks or features to work on in the future',
           'Backlog tickets can include new features, bug fixes, and other tasks that need to be completed on in the future',
           ['CHORE']
         );
 
-        const todoBoard = await createNewBoard(
-          'To Do',
-          'Tickets awaiting to be worked on',
-          workspace._id
-        );
-        await createNewTicket(
-          todoBoard._id,
-          'Tasks or features not yet started',
-          'To do tickets can include new features, bug fixes, improvements to existing functionality, that need to be completed but have not yet been started',
-          ['FEAT']
-        );
+        await Workspace.findByIdAndUpdate(workspace._id, {
+          $push: { boards: new Types.ObjectId(backlogBoard._id) },
+        });
 
-        const inProgressBoard = await createNewBoard(
-          'In Progress',
-          'Tickets currently in progress',
-          workspace._id
-        );
-        await createNewTicket(
-          inProgressBoard._id,
-          'Tasks or features currently being worked on',
-          'In progress tickets can include new features, bug fixes, improvements to existing functionality, that are currently being worked on',
-          ['WIP']
-        );
+        await Board.findByIdAndUpdate(backlogBoard._id, {
+          $push: { tickets: new Types.ObjectId(ticket._id) },
+        });
 
-        const doneBoard = await createNewBoard(
-          'Done',
-          'Tickets that are done',
-          workspace._id
-        );
-        await createNewTicket(
-          doneBoard._id,
-          'Tasks or features that are done',
-          'Done tickets can include new features, bug fixes, improvements to existing functionality, that are done',
-          []
-        );
+        // const todoBoard = await createNewBoard(
+        //   'To Do',
+        //   'Tickets awaiting to be worked on',
+        //   workspace._id
+        // );
+        // await createNewTicket(
+        //   todoBoard._id,
+        //   'Tasks or features not yet started',
+        //   'To do tickets can include new features, bug fixes, improvements to existing functionality, that need to be completed but have not yet been started',
+        //   ['FEAT']
+        // );
+
+        // const inProgressBoard = await createNewBoard(
+        //   'In Progress',
+        //   'Tickets currently in progress',
+        //   workspace._id
+        // );
+        // await createNewTicket(
+        //   inProgressBoard._id,
+        //   'Tasks or features currently being worked on',
+        //   'In progress tickets can include new features, bug fixes, improvements to existing functionality, that are currently being worked on',
+        //   ['WIP']
+        // );
+
+        // const doneBoard = await createNewBoard(
+        //   'Done',
+        //   'Tickets that are done',
+        //   workspace._id
+        // );
+        // await createNewTicket(
+        //   doneBoard._id,
+        //   'Tasks or features that are done',
+        //   'Done tickets can include new features, bug fixes, improvements to existing functionality, that are done',
+        //   []
+        // );
         break;
 
       case 'basic':
